22466b149276a59188de5d389b1c4e6cc8e96f4e
Mahbub Shafin <mahbub86@my.yorku.ca>
2025-05-24 17:43:16 -0400

    commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: 'https://bey-bazaar.vercel.app',
   generateRobotsTxt: true,
   exclude: ["/profile", "/checkout", "/cart", "/update-password"],
-  sitemapSize: 7000,
+  sitemapSize: 5000,
   robotsTxtOptions: {
     policies: [
       { userAgent: '*', allow: '/', disallow: ['/profile', '/checkout', '/cart', '/update-password'] },
