0f285362f5f3663ace04b00f23e93e2d31f2fc7e
Mahbub Shafin <mahbub86@my.yorku.ca>
2025-07-29 17:04:05 -0400

    commit

--- src/app/api/checkout/route.tsx
+++ src/app/api/checkout/route.tsx
@@ -2,9 +2,8 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { supabase } from "../../lib/supabaseClient";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 export async function POST(req: Request) {
   try {
